eaa300f0ab0c0c4a4b378ae1f0fff8d58569a3be
Nanioi <0802ny@gmail.com>
2022-02-18 12:55:34 +0900

    리스트 화면구현, 상세화면 구현
    - 리스트 화면 구현
    - 상세화면 구현

--- TodoList/app/src/main/java/com/nanioi/todolist/data/entity/ToDoEntity.kt
+++ TodoList/app/src/main/java/com/nanioi/todolist/data/entity/ToDoEntity.kt
@@ -1,9 +1,13 @@
 package com.nanioi.todolist.data.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
 data class ToDoEntity(
-    val id: Long = 0,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val title: String,
     val description: String,
     val hasCompleted: Boolean = false
 )
+
--- TodoList/app/src/main/java/com/nanioi/todolist/data/repository/DefaultToDoRepository.kt
+++ TodoList/app/src/main/java/com/nanioi/todolist/data/repository/DefaultToDoRepository.kt
@@ -1,36 +1,40 @@
 package com.nanioi.todolist.data.repository
 
 import com.nanioi.todolist.data.entity.ToDoEntity
+import com.nanioi.todolist.data.local.db.dao.ToDoDao
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
-class DefaultToDoRepository: ToDoRepository {
+class DefaultToDoRepository(
+    private val toDoDao: ToDoDao,
+    private val ioDispatcher: CoroutineDispatcher
+): ToDoRepository {
 
-    override suspend fun getToDoList(): List<ToDoEntity> {
-        TODO()
+    override suspend fun getToDoItem(id: Long): ToDoEntity? = withContext(ioDispatcher) {
+        toDoDao.getById(id)
     }
 
-    override suspend fun insertToDoItem(toDoEntity: ToDoEntity) :Long {
-        TODO()
+    override suspend fun getToDoList(): List<ToDoEntity> = withContext(ioDispatcher) {
+        toDoDao.getAll()
     }
 
-    override suspend fun insertToDoList(toDoList: List<ToDoEntity>) {
-        TODO()
+    override suspend fun insertToDoItem(toDoEntity: ToDoEntity): Long = withContext(ioDispatcher) {
+        toDoDao.insert(toDoEntity)
     }
 
-    override suspend fun updateTodoItem(toDoEntity: ToDoEntity):Boolean {
-        TODO("Not yet implemented")
+    override suspend fun insertToDoList(toDoList: List<ToDoEntity>) = withContext(ioDispatcher) {
+        toDoDao.insert(toDoList)
     }
 
-    override suspend fun getToDoItem(id: Long): ToDoEntity{
-        TODO()
+    override suspend fun updateTodoItem(toDoEntity: ToDoEntity): Boolean = withContext(ioDispatcher) {
+        toDoDao.update(toDoEntity)
     }
 
-    override suspend fun deleteAll() {
-        TODO("Not yet implemented")
+    override suspend fun deleteToDoItem(id: Long) :Boolean = withContext(ioDispatcher) {
+        toDoDao.delete(id)
     }
 
-    override suspend fun deleteToDoItem(id: Long) :Boolean {
-        TODO("Not yet implemented")
+    override suspend fun deleteAll() = withContext(ioDispatcher) {
+        toDoDao.deleteAll()
     }
 }
